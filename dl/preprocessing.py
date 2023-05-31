29913d52d97f37efcc94fe595626c493937c1859
sidjakinsboriss <sidjakinsboriss@gmail.com>
2023-05-31 15:16:00 +0200

    matrix code

--- dl/preprocessing.py
+++ dl/preprocessing.py
@@ -1,5 +1,12 @@
+import json
 import os
 import re
+from collections import Counter
+
+from text_preprocessing import preprocess_text
+from text_preprocessing import to_lower, remove_email, remove_url, remove_punctuation, lemmatize_word, \
+    remove_phone_number, remove_itemized_bullet_and_numbering, expand_contraction, remove_special_character, \
+    remove_whitespace, normalize_unicode, remove_stopword, remove_name
 
 import nltk
 import numpy as np
@@ -8,7 +15,6 @@ import pandas as pd
 from bs4 import BeautifulSoup
 from gensim.models import KeyedVectors
 from matplotlib import pyplot as plt
-from nltk import word_tokenize
 from nltk.corpus import stopwords
 from nltk.stem import WordNetLemmatizer
 
@@ -17,6 +23,8 @@ class PreProcessor:
     def __init__(self, df: pandas.DataFrame):
         self.df = df
         self.relevant_labels = ['process', 'existence', 'technology', 'property', 'not-ak']
+        self.lemmatizer = WordNetLemmatizer()
+        self.word_vect = KeyedVectors.load_word2vec_format("SO_vectors_200.bin", binary=True)
         self.reply_patterns = [
             re.compile(
                 "On ((Mon|Tue|Wed|Thu|Fri|Sat|Sun), )?"
@@ -32,7 +40,7 @@ class PreProcessor:
             re.compile("On \\d+/\\d+/\\d+ \\d+:\\d+, .+ wrote:"),
             re.compile("On \\d+/\\d+/\\d+ \\d+:\\d+:\\d+ .+ wrote:"),
             re.compile(".+ hat am \\d+\\.\\d+\\.\\d+ \\d+:\\d+ geschrieben:"),
-            re.compile("From:[\\S\\s]*Date:[\\S\\s]*To:[\\S\\s]*Subject:", re.MULTILINE),
+            re.compile("From:[\\S\\s]*Date:[\\S\\s]*To:[\\S\\s]*Subject:", re.MULTILINE)
         ]
 
     def remove_embedded(self, raw: str) -> str:
@@ -67,11 +75,11 @@ class PreProcessor:
 
     def lemmatize_sentence(self, sentence: str) -> str:
         words = nltk.word_tokenize(sentence)
-        lemmatized_words = [WordNetLemmatizer().lemmatize(word) for word in words]
+        lemmatized_words = [self.lemmatizer.lemmatize(word) for word in words]
         return ' '.join(lemmatized_words)
 
     def sentence_vector(self, sentence: str, model):
-        words = sentence.split()
+        words = nltk.word_tokenize(sentence)
         vectors = [model[word] for word in words if word in model.key_to_index]
         if (length := len(vectors)) <= 300:
             return vectors + [np.zeros(200) for _ in range(300 - length)]
@@ -80,7 +88,7 @@ class PreProcessor:
 
     def remove_labels(self, labels: list) -> str:
         filtered = [label for label in labels if label in self.relevant_labels]
-        if 'not-ak' in filtered and len(filtered) > 1: # This was the case for some reason
+        if 'not-ak' in filtered and len(filtered) > 1:  # This was the case for some reason
             return ''
         return ', '.join(filtered)
 
@@ -92,9 +100,6 @@ class PreProcessor:
         # get only the columns needed for training
         self.df = self.df[["SUBJECT", "BODY", "TAGS"]]
 
-        # need all emails to train word embedding
-        emails = df['BODY'].copy()
-
         # remove unnecessary labels
         self.df['TAGS'] = self.df['TAGS'].str.strip('[]')
         self.df["TAGS"] = self.df["TAGS"].transform(
@@ -104,30 +109,54 @@ class PreProcessor:
         # remove entries that have empty tags
         self.df = self.df[~(self.df['TAGS'] == "")]
 
-        # TODO: remove urls
-
         # parse HTML to plain_text and remove embedded threads
         self.df["CONTENT"] = self.df["BODY"].transform(
             lambda x: self.remove_embedded(BeautifulSoup(x, features="html.parser").get_text())
         )
 
-        # remove stop words
+        processing_function_list = [to_lower,
+                                    remove_url,
+                                    remove_email,
+                                    remove_phone_number,
+                                    remove_itemized_bullet_and_numbering,
+                                    expand_contraction,
+                                    remove_special_character,
+                                    remove_punctuation,
+                                    remove_whitespace,
+                                    normalize_unicode,
+                                    remove_stopword,
+                                    lemmatize_word]
+
         self.df["CONTENT"] = self.df["CONTENT"].transform(
-            lambda x: self.remove_stop_words(x)
+            lambda x: preprocess_text(x, processing_function_list)
         )
 
-        # lemmatization
-        self.df["CONTENT"] = self.df["CONTENT"].transform(
-            lambda x: self.lemmatize_sentence(x)
-        )
-
-        # word embedding
-        tokenized_sentences = [word_tokenize(sentence) for sentence in emails.tolist()]
-        # model = Word2Vec(sentences=tokenized_sentences, window=5, min_count=2, workers=4, epochs=50)
-        word_vect = KeyedVectors.load_word2vec_format("SO_vectors_200.bin", binary=True)
-        self.df["CONTENT"] = self.df["CONTENT"].apply(lambda x: self.sentence_vector(x, word_vect))
-
+        # # Remove URLs
+        # url_pattern = r'(https?://\S+)'
+        # self.df['CONTENT'] = self.df['CONTENT'].str.replace(url_pattern, '', regex=True)
+        #
+        # # remove stop words
+        # self.df["CONTENT"] = self.df["CONTENT"].transform(
+        #     lambda x: self.remove_stop_words(x)
+        # )
+        #
+        # # lemmatization
+        # self.df["CONTENT"] = self.df["CONTENT"].transform(
+        #     lambda x: self.lemmatize_sentence(x)
+        # )
+        #
+        # # to lower case
+        # self.df["CONTENT"] = self.df["CONTENT"].transform(
+        #     lambda x: x.lower()
+        # )
+        #
         self.df = self.df[["CONTENT", "TAGS"]]
+
+    def word_embedding(self):
+        # tokenized_sentences = [word_tokenize(sentence) for sentence in emails.tolist()]
+        # model = Word2Vec(sentences=tokenized_sentences, window=5, min_count=2, workers=4, epochs=50)
+
+        self.df["CONTENT"] = self.df["CONTENT"].apply(lambda x: self.sentence_vector(x, self.word_vect))
 
     def plot_email_word_counts(self):
         length_ranges = [x * 100 for x in range(60)]
@@ -144,12 +173,37 @@ class PreProcessor:
 
         self.df = self.df.drop(['email_length', 'length_range'], axis=1)
 
+    def count_word_occurrences(self):
+        text = ' '.join(self.df['CONTENT'].values)
+
+        words = text.split()
+        word_counts = Counter(words)
+        sorted_word_counts = sorted(word_counts.items(), key=lambda x: x[1], reverse=True)
+
+        word_counts_dict = {word: count for word, count in sorted_word_counts}
+
+        # Export the word counts to JSON
+        with open('word_counts.json', 'w') as f:
+            json.dump(word_counts_dict, f, indent=4)
+
+    def show_words_absent_from_word_embedding(self):
+        email_words = list(set(' '.join(self.df['CONTENT'].values).split()))
+        model_words = self.word_vect.index_to_key
+
+        with open('words.txt', 'w', encoding="utf-8") as f:
+            for word in email_words:
+                if word not in model_words:
+                    f.write(f"{word}\n")
+
     def export_to_json(self):
         self.df.to_json(os.path.join(os.getcwd(), "../data/preprocessed.json"), index=True, orient='index', indent=4)
 
 
 if __name__ == "__main__":
-    df = pd.read_csv("../data/input.csv")
+    df = pd.read_csv("../data/data.csv")
     pre_processor = PreProcessor(df)
     pre_processor.pre_process()
+    # pre_processor.show_words_absent_from_word_embedding()
+    # pre_processor.count_word_occurrences()
+    pre_processor.word_embedding()
     pre_processor.export_to_json()
--- dl/training.py
+++ dl/training.py
@@ -1,8 +1,11 @@
+import json
 import os
 
 import numpy as np
-import pandas
+import pandas as pd
 import torch
+from matplotlib import pyplot as plt
+from sklearn.metrics import precision_score, accuracy_score, recall_score
 
 from dl.dataset_split_handler import DatasetSplitHandler
 from dl.rnn import EmailRNN
@@ -37,15 +40,17 @@ def train(model, train_loader, valid_loader, test_loader, criterion, optimizer, 
         print(f"Epoch {epoch + 1}/{num_epochs}: Validation Loss = {valid_loss:.4f}")
 
     # After training, evaluate the final model on the testing set
-    accuracy, test_loss = evaluate(model, test_loader, criterion)
+    precision, recall, accuracy = evaluate(model, test_loader, criterion)
     print(f"Accuracy = {accuracy:.4f}")
 
 
 def evaluate(model, test_loader, criterion):
     model.eval()
     total_loss = 0.0
-    correct = 0
-    total = 0
+
+    predicted = []
+    ground_truth = []
+
     with torch.no_grad():
         for inputs, labels in test_loader:
             outputs = model(inputs)
@@ -54,24 +59,89 @@ def evaluate(model, test_loader, criterion):
 
             outputs = torch.sigmoid(outputs)
             threshold = 0.6  # Adjust this threshold as needed
-            predicted = (outputs >= threshold).int()
+
+            predicted.append((outputs >= threshold).int().cpu().numpy())
+            ground_truth.append(labels.cpu().numpy())
 
             # Convert tensors to numpy arrays
-            predicted = predicted.cpu().numpy()
-            labels = labels.cpu().numpy()
+            # labels += labels.cpu().numpy()
 
-            for i in range(len(labels)):
-                total += 1
-                if np.array_equal(predicted[i], labels[i]):
-                    correct += 1
+    predicted = np.concatenate(predicted, axis=0)
+    ground_truth = np.concatenate(ground_truth, axis=0)
 
-    accuracy = correct / total
+    matrix(ground_truth, predicted)
+
+    # Calculate precision, accuracy, and recall
+    precision = precision_score(ground_truth, predicted, average='micro')
+    accuracy = accuracy_score(ground_truth, predicted)
+    recall = recall_score(ground_truth, predicted, average='micro')
+
     avg_loss = total_loss / len(test_loader)
-    return accuracy, avg_loss
+    return precision, recall, accuracy
+
+
+def matrix(ground_truth: np.ndarray, predicted: np.ndarray):
+    row_names = col_names = np.array(['existence', 'not-ak', 'process', 'property', 'technology'])
+
+    row_labels = np.unique(ground_truth, axis=0)
+    column_labels = np.unique(predicted, axis=0)
+
+    # Expand ground_truth labels
+    for label in row_labels:
+        indices = np.where(label == 1)[0]
+        new_label = ', '.join([row_names[i] for i in indices])
+        if new_label and new_label not in row_names:
+            row_names = np.append(row_names, new_label)
+
+    # Expand predicted labels
+    for label in column_labels:
+        indices = np.where(label == 1)[0]
+        new_label = ', '.join([col_names[i] for i in indices])
+        if new_label and new_label not in col_names:
+            col_names = np.append(col_names, new_label)
+
+    matrix = np.zeros((len(row_names), len(col_names)))
+
+    for i, truth in enumerate(ground_truth):
+        pred = predicted[i]
+
+        indices = np.where(truth == 1)[0]
+        row_label = ', '.join([row_names[i] for i in indices])
+        row_index = np.where(row_names == row_label)[0][0]
+
+        indices = np.where(pred == 1)[0]
+        col_label = ', '.join([col_names[i] for i in indices])
+
+        if col_label:
+            col_index = np.where(col_names == col_label)[0][0]
+
+            matrix[row_index][col_index] += 1
+
+    num_rows, num_cols = matrix.shape
+    fig, ax = plt.subplots()
+
+    ax.set_xticks(range(len(col_names)))
+    ax.set_xticklabels(col_names, rotation=90, ha='right')
+    ax.set_yticks(range(len(row_names)))
+    ax.set_yticklabels(row_names)
+
+    # Create the heatmap
+    heatmap = ax.matshow(matrix, cmap='Purples')
+    fig.colorbar(heatmap)
+
+    # Annotate the matrix values on the heatmap
+    for i in range(num_rows):
+        for j in range(num_cols):
+            ax.text(j, i, str(matrix[i, j]), va='center', ha='center')
+
+    # Save the plot
+    plt.savefig("matrix.jpg", bbox_inches='tight')
 
 
 if __name__ == "__main__":
-    df = pandas.read_json(os.path.join(os.getcwd(), "../data/preprocessed.json"), orient='index')
+    data = json.load(open(os.path.join(os.getcwd(), "../data/preprocessed.json"), "r"))
+    df = pd.DataFrame.from_dict(data, orient="index")
+    # df = pandas.read_json(os.path.join(os.getcwd(), "../data/preprocessed.json"), orient='index')
     dataset_split_handler = DatasetSplitHandler(df)
     dataset_split_handler.encode_labels()
     dataset_split_handler.split_dataset()
